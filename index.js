634a6bd6f94e281865ffbc18573d685ca28133a0
Alexey19800506 <osiyuk19800506@gmail.com>
2023-07-31 13:49:07 +0300

    t2

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const rootElement = document.querySelector('#root');
 const elem = React.createElement(
     'div', { className: 'greeting' },
     React.createElement(
-        'div', { className: 'greeting__title' },
+        'h1', { className: 'greeting__title' },
         'Hello, world!'
     ),
     React.createElement(
-        'div', { className: 'greeting__text' },
+        'p', { className: 'greeting__text' },
         'I\'m learning React!'
     ),
 );
